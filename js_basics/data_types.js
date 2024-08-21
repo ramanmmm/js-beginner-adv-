aa23f1f65780a8e82d21e07730c2c40c990568ac
RAMAN MISHRA <108969053+ramanmmm@users.noreply.github.com>
2024-08-21 11:36:29 +0000

    data types and all

---
+++ js_basics/data_types.js
@@ -0,0 +1,12 @@
+"use strict";  // treat all js code as newer version
+let a=2
+a++
+console.log(a++ +" "+ ++a+" "+ --a+" "+ ++a);
+console.log(typeof undefined) // o/p is undefined
+console.log(typeof null)//.  o/p is object
+console.log(typeof a) // number or integer
+let b = true //boolean
+let name = "kill"
+let radius = 2.05;
+console.log(typeof name)
+console.log(typeof radius +" "+radius)
---
+++ js_basics/js_variables.js
@@ -0,0 +1,10 @@
+let account = 22222
+let name = "raman"
+const roll=1
+var temp="1111";
+console.table([account,name,roll,temp])
+/*
+var is not suggested to use it is not use in modern coding problems 
+let is mutable 
+const is immutable
+*/
